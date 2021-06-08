24cb5c0e90c11f47744196738311025d9c21bd50
akikoinhd <akikoinhd@gmail.com>
2021-06-07 19:38:30 -0500

    material-ui dependencies

--- client/components/MainContainer.jsx
+++ client/components/MainContainer.jsx
@@ -9,14 +9,14 @@ class MainContainer extends Component {
         console.log("MainContainer rendering, bud.");
         return (
             <div>
-                <h1>Login Here</h1>, 
+                <h1>Login Here</h1>
                 <div id="login">
                     <input id="username" placeholder="username"></input>
                     <input id="password" placeholder="password" type="password"></input>
                     <button id="button" >Submit</button>
                 </div>
             </div>
-        )
+        );
     };
 }
 export default MainContainer;
