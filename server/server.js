3c20b760897b7498297705c8e15eca1fc10b3446
Whit Rooke <whit.rooke@gmail.com>
2021-06-09 19:21:53 -0400

    pre pull commit

--- server/server.js
+++ server/server.js
@@ -73,19 +73,24 @@ io.on("connection", socket => {
     Player3: null,
     Player4: null
   };
+  //generation of the player list, should happen upon entry into game board,
+  //as more people enter the game board, more populate lobby
   socket.on("enter", (displayName) =>{
     if(!playerList.Player1) playerList.Player1 = displayName;
     else if(!playerList.Player2) playerList.Player2 = displayName;
     else if(!playerList.Player3) playerList.Player3 = displayName;
     else if(!playerList.Player4) playerList.Player4 = displayName;
+    io.sockets.emit(playerList);
   });
 
   //listener for the data from an answered question to all other users
+  //should fire from the answering of a question correctly
   socket.on("answer", (answerData)=>{
     //broadcast to all other clients the data
     io.broadcast.emit("clientAnswer", answerData);
   })
   //listener for the question data to tell other user's questions
+  //should fire when a question is picked
   socket.on("questionPick", (questionInfo) => {
     io.broadcast.emit("clientQuestionPick", questionInfo);
   })
